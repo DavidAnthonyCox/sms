8d2734356756b5e43ce1a5838a3784d21022da34
Rab Beverly <rab@robertbeverly.com>
2015-05-14 13:14:19 -0400

    People now have phones. Currently you have to add a contact_category first or the form will fail at the database level (since you can't input one if there are no contact categories to select from).

--- app/admin/person.rb
+++ app/admin/person.rb
@@ -4,7 +4,7 @@ require 'andand'
 ActiveAdmin.register Person do
   permit_params :firstname, :middlename, :lastname, 
     :identities, :identities_attributes => [:id, :value, :identity_type, :person_id, :organization_id, :_destroy],
-    :phones_attributes => [:id, :person_id, :contact_category, :area_code, :number, :extension, :note, :_destroy]
+    :phones_attributes => [:id, :person_id, :contact_category_id, :area_code, :number, :extension, :note, :_destroy]
   
   # @person = Person.find(params[:id])
   # controller do
@@ -20,7 +20,14 @@ ActiveAdmin.register Person do
 
       column do
         panel "Phones" do
-          # table_for
+          table_for person.phones do
+            column :type do |this_phone|
+              this_phone.contact_category.name
+            end #type
+            column :number do |this_phone|
+              this_phone.area_code.to_s + " " + this_phone.number.to_s
+            end #number
+          end
         end
         panel "Addresses" do
           "Adresses\nAddresses"
@@ -50,26 +57,44 @@ ActiveAdmin.register Person do
 
   form do |f|
 
-    f.inputs 'Name' do
-      f.input :firstname
-      f.input :middlename
-      f.input :lastname
-    end
+    columns do
 
-    f.has_many :identities, heading: "Government / Organizational IDs", allow_destroy: true do |instance|
-      instance.input :organization
-      instance.input :identity_type, 
-        :as => :select, :collection => options_for_select([["SSN","SSN"],
-          ["License","License"],["Other","Other"]], instance.object.andand.identity_type)
+      column do
 
-      instance.input :value
-    end
+        f.inputs 'Name' do
+          f.input :firstname
+          f.input :middlename
+          f.input :lastname
+        end
 
-    # f.panel 'Organizational Identities' do
-    #   f.has_many :identities, :label => 'Intsldkfjskdfj' do |instance|
-    #     instance.inputs 'Inputses My Precious'
-    #   end
-    # end
+        panel "Phones" do
+          f.has_many :phones, heading: nil, allow_destroy:true do |instance|
+            instance.input :contact_category
+            instance.input :area_code
+            instance.input :number
+            instance.input :extension
+            instance.input :note
+          end #phones
+        end #panel phones
+
+      end #column
+
+      column do
+
+        panel "Government / Organizational IDs" do
+          f.has_many :identities, heading: nil, allow_destroy: true do |instance|
+            instance.input :organization
+            instance.input :identity_type, 
+              :as => :select, :collection => options_for_select([["SSN","SSN"],
+                ["License","License"],["Other","Other"]], instance.object.andand.identity_type)
+            instance.input :value
+          end #identities
+        end #panel
+
+      end #column
+      
+
+    end #columns
 
     para "Press cancel to return to the list without saving."
     f.actions
--- app/admin/phone.rb
+++ app/admin/phone.rb
@@ -1,9 +1,9 @@
-ActiveAdmin.register ContactCategory do
+ActiveAdmin.register Phone do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :id, :name, :description
+# permit_params :list, :of, :attributes, :on, :model
 #
 # or
 #

5e8a0ac704ba625d7cdce713945d7b60634b98b7
Rab Beverly <rab@robertbeverly.com>
2015-05-16 13:46:14 -0400

    Added some models, removed the non-general idea of "license", added a sketch of DB ideas to implement. WIP.

--- app/models/license.rb
+++
@@ -1,9 +0,0 @@
-class License < ActiveRecord::Base
-    validates :name, :description, presence: true
-    validates :name, uniqueness: true
-    default_scope { order('name') }
-
-    has_many :course_purpose
-    has_many :license_held
-    has_one :organization, :foreign_key => "issuer_id"
-end
--- app/models/license_held.rb
+++
@@ -1,5 +0,0 @@
-class LicenseHeld < ActiveRecord::Base
-  belongs_to :license
-  belongs_to :person
-  default_scope { order('license_number') }
-end
